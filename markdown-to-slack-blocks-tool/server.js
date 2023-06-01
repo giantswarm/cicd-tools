8cebf6e29f529496fe2d55919ea8b8172394051b
ericgraf <eric@giantswarm.io>
2023-06-01 09:42:57 -0400

    Update markdown-to-slack-blocks-tool/server.js
    Co-authored-by: Marcus Noble <AverageMarcus@users.noreply.github.com>

--- markdown-to-slack-blocks-tool/server.js
+++ markdown-to-slack-blocks-tool/server.js
@@ -3,6 +3,7 @@ const mack = require('@tryfabric/mack');
 async function run() {
   const blocks = await mack.markdownToBlocks(process.env.INPUT.replace("\\n", "\n"));
   console.log(blocks); 
+  process.exit(0);
 }
 
 run()
