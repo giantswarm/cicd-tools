0204ff3e4509adf975952c1a37d0502adae5bcd0
ericgraf <eric@giantswarm.io>
2023-06-06 09:34:36 -0400

    Add ping support

--- markdown-to-slack-blocks-tool/server.js
+++ markdown-to-slack-blocks-tool/server.js
@@ -1,9 +1,18 @@
 const mack = require('@tryfabric/mack');
 
 async function run() {
-  const blocks = await mack.markdownToBlocks(process.env.INPUT.replace("\\n", "\n"));
-  console.log(blocks); 
-  process.exit(0);
+  let blocks = await mack.markdownToBlocks(process.env.INPUT.replace("\\n", "\n"));
+  blocks = blocks.map(b => {
+    if (b.text && b.text.text) {
+      b.text.text = b.text.text
+          .replace(/&lt;@([A-Za-z0-9]+)&gt;/g, "<@$1>") // User mentions
+          .replace(/&lt;!([A-Za-z0-9]+\^[A-Za-z0-9]+)&gt;/g, "<!$1>") // Group mentions
+          .replace(/&lt;!([A-Za-z0-9\|]+)&gt;/g, "<!$1>") // Special mentions
+          .replace(/&lt;#([A-Za-z0-9]+)&gt;/g, "<#$1>"); // Channels
+    }
+    return b;
+  })
+  console.log(blocks);
 }
 
-run()
+run();
