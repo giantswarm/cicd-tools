de1f750e0e2602a71d169d943a3ae8555f54ef4b
ericgraf <eric@giantswarm.io>
2023-06-13 07:48:48 -0400

    Update markdown-to-slack-blocks-tool/server.js
    Co-authored-by: Marcus Noble <AverageMarcus@users.noreply.github.com>

--- markdown-to-slack-blocks-tool/server.js
+++ markdown-to-slack-blocks-tool/server.js
@@ -1,18 +1,23 @@
 const mack = require('@tryfabric/mack');
 
 async function run() {
-  let blocks = await mack.markdownToBlocks(process.env.INPUT.replace("\\n", "\n"));
+  let input = process.env.INPUT
+      .replace("\\n", "\n")
+      .replace(/<@([A-Za-z0-9]+)>/g, "&lt;@$1&gt;") // User mentions
+      .replace(/<!([A-Za-z0-9\|\^]+)>/g, "&lt;!$1&gt;") // Group / Special mentions
+      .replace(/<#([A-Za-z0-9]+)>/g, "&lt;#$1&gt;") // Channels
+    
+  let blocks = await mack.markdownToBlocks(input);
   blocks = blocks.map(b => {
     if (b.text && b.text.text) {
       b.text.text = b.text.text
           .replace(/&lt;@([A-Za-z0-9]+)&gt;/g, "<@$1>") // User mentions
-          .replace(/&lt;!([A-Za-z0-9]+\^[A-Za-z0-9]+)&gt;/g, "<!$1>") // Group mentions
-          .replace(/&lt;!([A-Za-z0-9\|]+)&gt;/g, "<!$1>") // Special mentions
-          .replace(/&lt;#([A-Za-z0-9]+)&gt;/g, "<#$1>"); // Channels
+          .replace(/&lt;!([A-Za-z0-9\|\^]+)&gt;/g, "<!$1>") // Group / Special mentions
+          .replace(/&lt;#([A-Za-z0-9]+)&gt;/g, "<#$1>") // Channels
     }
     return b;
   })
-  console.log(JSON.stringify(blocks));
+  console.log(JSON.stringify(blocks)); 
 }
 
 run();
